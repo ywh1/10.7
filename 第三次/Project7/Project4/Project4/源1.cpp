4ea1f6e70deeed160d9ea040ac96312ad460c6d8
ywh1 <43868925+ywh1@users.noreply.github.com>
2018-10-17 07:29:30 +0800

    十个数中最大值

--- 第三次/Project7/Project4/Project4/源1.cpp
+++ 第三次/Project7/Project4/Project4/源1.cpp
@@ -4,16 +4,16 @@ int main()
 {
 	int i, b;
 	printf("请输入十个整数：");
-	int max[20] = { 8, 7, 6, 9, 5, 4, 34, 75, 88, 46 };
+	int max[20] = {8,7,6,9,5,4,34,75,88,46};
 	for (i = 0; i < 10; i++)
-		printf("%d  ", max[i]);
+	   printf ("%d  ", max[i]); 
 	for (i = 0; i < 9; i++)
-	if (max[i] >= max[i + 1])
-	{
-		b = max[i];
-		max[i] = max[i + 1];
-		max[i + 1] = b;
-	}
+		if (max[i] >= max[i + 1])
+		{
+			b = max[i];
+			max[i] = max[i + 1];
+			max[i + 1] = b;
+		}
 	printf("\n十个数中最大的是：%d \n ", max[i]);
 	system("pause");
 	return 0;
